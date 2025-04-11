bc0583fb6902c3a88e045f26367f39d9e0c284ea
sigeyh <rsigeyh28@gmail.com>
2025-04-11 10:53:17 +0300

    Add files via upload

---
+++ service woker.js
@@ -0,0 +1,24 @@
+// sw.js
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('my-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/style.css',
+          '/script.js',
+          '/icons/icon-192x192.png',
+          '/icons/icon-512x512.png',
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        return cachedResponse || fetch(event.request);
+      })
+    );
+  });
+  
